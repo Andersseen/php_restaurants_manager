8cd23fe5ae8414038cc7bc3d3d667af1c23c9f90
andrey <forestx24@gmail.com>
2021-03-26 11:57:24 +0100

    change styles

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ date_default_timezone_set('Europe/Madrid');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
-    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/main.css">
 </head>
 
 <body>
